723f5623fa228b0c94511224af804bee9b6c145e
Aramis Diaz <aramisdiaz@mail.usf.edu>
2020-09-16 17:30:25 -0700

    Go back pls

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 
+
 // Defining methods for the recipesController
 module.exports = {
   findAll: function(req, res) {
--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 // Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 
 
 // Serve up static assets (usually on heroku)
@@ -16,10 +23,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-app.use(routes)
+app.use(routes);
+
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.REACT_APP_MONGODB_URI || "mongodb://localhost/plentyofthyme", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plentyofthyme", { useNewUrlParser: true });
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
